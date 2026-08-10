212ef58ef70ffce3853955b5caf77ddd3af7fc42
bounsongphommasen56-bit <bounsongphommasen56@gmail.com>
2026-08-10 16:31:53 +0800

    debug market

--- src/lib/marketApi.js
+++ src/lib/marketApi.js
@@ -3,12 +3,12 @@ import marketData from "../data/marketData";
 const API_URL = "/api/markets";
 
 export async function getMarkets() {
-
   try {
-
     const res = await fetch(API_URL);
 
     const json = await res.json();
+
+    console.log("API Response:", json);
 
     if (!json.success) {
       throw new Error("API Error");
@@ -16,43 +16,36 @@ export async function getMarkets() {
 
     const backendMarkets = json.data;
 
-    return marketData.map((item) => {
+    console.log("backendMarkets:", backendMarkets);
 
+    const result = marketData.map((item) => {
       const api = backendMarkets.find(
         (m) => m.code === item.code
       );
 
       if (!api) {
-
         return {
           ...item,
           price: 0,
           change: 0,
           status: "offline",
         };
-
       }
 
       return {
-
         ...item,
-
         price: api.price,
-
         change: api.change,
-
         status: "online",
-
       };
-
     });
 
+    console.log("result:", result);
+
+    return result;
+
   } catch (err) {
-
-    console.error(err);
-
+    console.error("MARKET ERROR:", err);
     return [];
-
   }
-
 }
